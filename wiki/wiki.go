97328c02397cc374995e6a086c2838904400d049
paul.oconnell <paul.oconnell@bearingpoint.com>
2017-05-16 14:05:01 +0100

    add http server library

--- wiki/wiki.go
+++ wiki/wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+  "net/http"
 	"io/ioutil"
 )
 
@@ -24,9 +25,15 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+    //p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+    //p1.save()
+    //p2, _ := loadPage("TestPage")
+    //fmt.Println(string(p2.Body))
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
